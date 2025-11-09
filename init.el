9e54f0ff6ae25bae108e36840d7b5d85355cb4cb
Rahul Raj <rahulraj@Rahuls-MacBook-Pro-4.local>
2025-11-09 11:12:09 +0530

    Added claude code el

--- init.el
+++ init.el
@@ -840,6 +840,26 @@
 
 (use-package vterm :ensure t)
 
+
+;; install required inheritenv dependency:
+(use-package inheritenv
+  :vc (:url "https://github.com/purcell/inheritenv" :rev :newest))
+
+(use-package claude-code :ensure t
+  :vc (:url "https://github.com/stevemolitor/claude-code.el" :rev :newest)
+  :config
+  ;; optional IDE integration with Monet
+  ;;(add-hook 'claude-code-process-environment-functions #'monet-start-server-function)
+  ;;(monet-mode 1)
+
+  (claude-code-mode)
+  :bind-keymap ("C-c c" . claude-code-command-map)
+
+  ;; Optionally define a repeat map so that "M" will cycle thru Claude auto-accept/plan/confirm modes after invoking claude-code-cycle-mode / C-c M.
+  :bind
+  (:repeat-map my-claude-code-map ("M" . claude-code-cycle-mode)))
+
+
 ;;(use-package combobulate
 ;;   :custom
 ;;   ;; You can customize Combobulate's key prefix here.
